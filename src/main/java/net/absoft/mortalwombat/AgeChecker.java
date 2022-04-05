1fa6132c00373a2757168c0d9cccd00a58620ed2
Ivan <i.m.krot@ukr.net>
2022-04-05 17:23:12 +0300

    Update MIN_AGE to 21

--- src/main/java/net/absoft/mortalwombat/AgeChecker.java
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -1,7 +1,8 @@
 package net.absoft.mortalwombat;
 
 public class AgeChecker {
-    private final int MIN_AGE = 18;
+    private final int MIN_AGE = 21;
+
     public boolean isAgeCorrect(int age) {
         return age >= MIN_AGE;
     }
