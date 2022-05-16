e6620b3c5d0320a4f96f6f583c149ef1b91df3e4
Ivan <i.m.krot@ukr.net>
2022-05-16 13:17:59 +0300

    Lesson 3 hometask (restore to the initial state)

--- src/main/java/net/absoft/mortalwombat/AgeChecker.java
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -1,8 +1,12 @@
 package net.absoft.mortalwombat;
 
 public class AgeChecker {
-    private final int MIN_AGE = 18;
+    private int minAge = 18;
+
+    public void setMinAge(int minAge) {
+        this.minAge = minAge;
+    }
     public boolean isAgeCorrect(int age) {
-        return age >= MIN_AGE;
+        return age >= minAge;
     }
 }
